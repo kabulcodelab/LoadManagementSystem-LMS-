ec3208ae53f84e65d0e4216207ac9c44331edd59
aref hakimi <arefhakimi2020@gmail.com>
2026-06-19 15:15:05 +0430

    second update of load pages

---
+++ wwwroot/Js/file-viewer.js
@@ -0,0 +1,6 @@
+﻿window.viewFile = function (byteArray, fileName, mimeType) {
+    const blob = new Blob([byteArray], { type: mimeType });
+    const url = URL.createObjectURL(blob);
+    window.open(url, '_blank');
+    setTimeout(() => URL.revokeObjectURL(url), 60000);
+};
